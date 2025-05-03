78d78c8b2763c87a75ba6d51afd766eb562fbab3
avaniausekar <avani1311a@gmail.com>
2025-05-03 21:24:47 +0530

    basic structure crawler

--- web-crawler/crawler.py
+++ web-crawler/crawler.py
@@ -1,17 +1,38 @@
-# pip3 install requests beautifulsoup4
-import requests
-from bs4 import BeautifulSoup
-import re
-import csv
 
-target_url = ""
-urls_to_visit = [target_url]
 
-# set a maximum crawl limit
-max_crawl = 10
+from collections import deque
+from locale import getdefaultlocale
 
-url_pattern = re.compile(r"/page/\d+/")
-product_data = []
+from bs4 import BeautifulSoup 
 
-def crawler():
-    pass
+# TODO saving in db, thread pools
+
+class Crawler(object):
+
+    def __init__(self, args):
+        self.depth = args.depth  
+        self.currentDepth = 1  
+        self.keyword = args.keyword.decode(getdefaultlocale()[1]) 
+        self.visitedUrls = set()   
+        self.unvisitedUrls = deque()    
+        self.unvisitedUrls.append(args.url) 
+        self.isCrawling = False
+
+    def start(self):
+        pass
+
+    def stop(self):
+        self.isCrawling = False
+
+    def getVisitedUrls(self):
+        return 
+
+    def assignDepthTasks(self):
+        pass
+
+    def _addUnvisitedUrls(self, webPage):
+        pass
+
+    def getAllUrls(self, url, pageSource):
+        urls = [] 
+        return urls
